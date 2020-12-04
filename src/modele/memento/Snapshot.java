49e4d143789a5c87071b64dabef6cd5524545eae
Tristan WATTIN <nekuharp@gmail.com>
2020-12-04 13:47:16 +0100

    Fix memento

--- src/modele/memento/Snapshot.java
+++ src/modele/memento/Snapshot.java
@@ -4,14 +4,17 @@ import java.io.Console;
 import java.util.Observable;
 
 import javafx.collections.ObservableList;
+import modele.personne.Client;
 import modele.pharmacie.Pharmacie;
 
 public class Snapshot {
 	private ObservableList<Pharmacie> listePharmaSaved;
+	private ObservableList<Client> listeClientSaved;
 	
-	public Snapshot(ObservableList<Pharmacie> listePharmaSaved) {
+	public Snapshot(ObservableList<Pharmacie> listePharmaSaved, ObservableList<Client> listeClientSaved) {
 		//System.out.println("Add " + listePharmaSaved + " to log");
 		this.setListePharmaSaved(listePharmaSaved);
+		this.setListeClientSaved(listeClientSaved);
 	}
 
 	public ObservableList<Pharmacie> getListePharmaSaved() {
@@ -20,5 +23,13 @@ public class Snapshot {
 
 	public void setListePharmaSaved(ObservableList<Pharmacie> listePharmaSaved) {
 		this.listePharmaSaved = listePharmaSaved;
+	}
+
+	public ObservableList<Client> getListeClientSaved() {
+		return listeClientSaved;
+	}
+
+	public void setListeClientSaved(ObservableList<Client> listeClientSaved) {
+		this.listeClientSaved = listeClientSaved;
 	}
 }
