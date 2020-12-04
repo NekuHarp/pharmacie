44c9a07fb4f513fa1b7864d154875c2c76f0ee83
Tristan WATTIN <nekuharp@gmail.com>
2020-12-04 14:28:12 +0100

    Liste Client sur Pharmacien Manager

--- src/modele/personne/PharmacienManager.java
+++ src/modele/personne/PharmacienManager.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 public class PharmacienManager extends Pharmacien {
 
     public ObservableList<Pharmacie> listePharmacie;
+    public ObservableList<Client> listeClient;
     public PharmacienManager instance;
 
     public PharmacienManager(String nom, String prenom, String adresse, double sal) {
@@ -14,10 +15,15 @@ public class PharmacienManager extends Pharmacien {
         this.setVentesTotal(0);
         instance = this;
         listePharmacie = FXCollections.observableArrayList();
+        listeClient = FXCollections.observableArrayList();
     }
 
     public void addPharma(Pharmacie x){
         this.listePharmacie.add(x);
         x.addEmployee(this);
+    }
+
+    public void addClient(Client x){
+        this.listeClient.add(x);
     }
 }
